1d0bd62ea2192096d8616716ecc58613cc9ac976
ramorim1998 <araujor920@gmail.com>
2021-10-11 21:18:10 -0300

    teste

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + './dist/cromai-desafio'));
+app.use(express.static(__dirname + '/dist/cromai-desafio'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'./dist/cromai-desafio/index.html'));
+res.sendFile(path.join(__dirname+'/dist/cromai-desafio/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
